b0b17920dccab3e6b02a4beea7c141a0a8be131c
pbentes <pedrogomes2000.3@gmail.com>
2024-08-31 14:32:14 +0100

    Added a script template for states

---
+++ script_templates/State/state.gd
@@ -0,0 +1,18 @@
+# meta-name: State script
+# meta-description: A script containing the basic definition of a state
+# meta-default: true
+# meta-space-indent: 4
+
+extends _BASE_
+
+func enter() -> void:
+	pass
+
+func exit() -> void:
+	pass
+
+func update(_delta: float) -> void:
+	pass
+	
+func physics_update(_delta: float) -> void:
+	pass
